f68fc10b1c92fd2bd77aff8baf2cf0480f292848
rostislavdolbilov <rostislavdolbilov@gmail.com>
2023-06-12 07:28:57 +0300

    readmy cleanup

--- src/main/java/com/exam/fileanalyzer/LogsAnalyzer.java
+++ src/main/java/com/exam/fileanalyzer/LogsAnalyzer.java
@@ -18,16 +18,7 @@ import java.util.zip.ZipFile;
 
 @Service
 public class LogsAnalyzer {
-	/**
-	 * Given a zip file, a search query, and a date range,
-	 * count the number of occurrences of the search query in each file in the zip file
-	 *
-	 * @param searchQuery  The string to search for in the file.
-	 * @param zipFile      The zip file to search in.
-	 * @param startDate    The start date of the search.
-	 * @param numberOfDays The number of days to search for.
-	 * @return A map of file names and the number of occurrences of the search query in the file.
-	 */
+
 	private final String DIRECTORY_PATH = "temporarydir/files";
 
 	public Map<String, Integer> countEntriesInZipFile(String searchQuery, File zipFile, LocalDate startDate, Integer numberOfDays) {
